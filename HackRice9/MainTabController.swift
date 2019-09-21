d53c9659d5907ee7d8c5159c59bd4c39bf1d537b
MyClean1998 <zl41@rice.edu>
2019-09-21 14:33:47 -0500

    main tab controller

---
+++ HackRice9/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  HackRice9
+//
+//  Created by uselessfatty on 9/21/19.
+//  Copyright © 2019 GoldenDumplings. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
