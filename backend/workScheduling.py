5e0a444fda21f7c7251ad0db06950f958e7946b1
JLiao980706 <liaofangshuo@gmail.com>
2019-09-21 12:13:55 -0500

    q learning agent completed

--- backend/workScheduling.py
+++ backend/workScheduling.py
@@ -2,10 +2,16 @@ import time
 
 
 class WorkSchedulingState:
+
     def __init__(self, pendingJobs, idleWorkers, idleEquipments, finishedJobs):
         self.pendingJobs = pendingJobs
         self.idleWorkers = idleWorkers
         self.idleEquipments = idleEquipments
         self.finishedJobs = finishedJobs
-
         self.time = time.time()
+    
+    def get_worker_job_pairs(self):
+        pass
+    
+    def get_a_features(self):
+        pass
