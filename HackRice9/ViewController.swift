31e1cfe885425f72f38b03220b8f1e35be8906b5
MyClean1998 <zl41@rice.edu>
2019-09-20 21:33:19 -0500

    Initial Commit

---
+++ HackRice9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackRice9
+//
+//  Created by uselessfatty on 9/20/19.
+//  Copyright © 2019 GoldenDumplings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
