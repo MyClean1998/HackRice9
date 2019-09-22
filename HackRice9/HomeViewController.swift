f7e8719bde004a5a640d4ee954d660d5f9d73c1b
Rguo18 <rg52@rice.edu>
2019-09-22 04:58:36 -0500

    homepage ui, textfield

--- HackRice9/HomeViewController.swift
+++ HackRice9/HomeViewController.swift
@@ -13,10 +13,6 @@ class HomeViewController: UIViewController {
     var tabbar: MainTabController!
     @IBOutlet weak var equipmentBox: UITextField!
     
-    @IBOutlet weak var dropBox: UIPickerView!
-    
-    var list = ["1", "2", "3"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tabbar = tabBarController as! MainTabController
@@ -24,47 +20,50 @@ class HomeViewController: UIViewController {
     
 //    private var scheduler: Scheduler!
     
+    @IBOutlet weak var taskEquipment: UITextField!
     @IBOutlet weak var workerNameField: UITextField!
     
+    @IBOutlet weak var workerShift: UITextField!
     @IBOutlet weak var workerLabel: UILabel!
     
     @IBOutlet weak var taskFacility: UITextField!
     
     //equipment selection
+//
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+//
+//        return list.count
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//
+//        self.view.endEditing(true)
+//        return list[row]
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//
+//        self.equipmentBox.text = self.list[row]
+//        self.dropBox.isHidden = true
+//    }
+//
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//
+//        if textField == self.equipmentBox {
+//            self.dropBox.isHidden = false
+//            //if you don't want the users to se the keyboard type:
+//
+//            textField.endEditing(true)
+//        }
+//    }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int{
-        return 1
-    }
 
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-
-        return list.count
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-
-        self.view.endEditing(true)
-        return list[row]
-    }
-
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-
-        self.equipmentBox.text = self.list[row]
-        self.dropBox.isHidden = true
-    }
-
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-
-        if textField == self.equipmentBox {
-            self.dropBox.isHidden = false
-            //if you don't want the users to se the keyboard type:
-
-            textField.endEditing(true)
-        }
-    }
     
     @IBOutlet weak var timeToComplete: UITextField!
-    
     @IBAction func AddWorker() {
         tabbar.addWorker(worker: Worker(equipment: [], name: workerNameField.text!, shifts: "morning"))
     }
