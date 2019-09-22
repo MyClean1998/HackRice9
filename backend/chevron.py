59a0a7662312b40e6d8fb4c37937ab8cd636ff74
YanjunChen329 <chenyanjun0329@outlook.com>
2019-09-21 20:24:13 -0500

    update chevron and state

--- backend/chevron.py
+++ backend/chevron.py
@@ -10,16 +10,15 @@ class Chevron:
 
         self.fac_loc = {}
         self.fac_equip_info = {}
-        self.work_state = WorkSchedulingState()
 
-        self.facilities = []
-        self.workers = []
-        self.work_orders = []
+        self.workOrder_id = 1
 
         self.load_facility_equipment_info(equipment_file, facility_file)
-        self.initialize_facility()
-        self.initialize_workers(worker_file)
-        self.
+        facilities = self.initialize_facility()
+        workers = self.initialize_workers(worker_file)
+        work_orders = self.initialize_work_orders(workOrder_file)
+        self.work_state = WorkSchedulingState(workers, facilities, work_orders)
+
 
     def load_facility_equipment_info(self, equipment_file, facility_file):
         equipment_df = pd.read_csv(equipment_file, header=1).iloc[:, 1:]
@@ -41,40 +40,47 @@ class Chevron:
             for idx, row in fac_equip_df.iterrows():
                 fac_equip[row["Equipment"]] = int(row[fac_name])
             self.fac_equip_info[fac_name] = fac_equip
-        
-    def update_envir(self, action):
-        # TODO: update the environment
-        pass
-
-
-        # print(self.equip_failure_prob, self.equip_fixing_time)
-        # print(self.fac_loc, self.fac_equip_info)
 
     def initialize_facility(self):
+        facilities = []
         for fac in self.fac_loc.keys():
             facility = Facility(fac, self.fac_loc[fac], self.fac_equip_info[fac], self.equip_failure_prob, self.equip_fixing_time)
-            self.facilities.append(facility)
-        # print(self.facilities)
+            facilities.append(facility)
+        return facilities
 
     def initialize_workers(self, worker_file):
+        workers = []
         worker_df = pd.read_csv(worker_file).iloc[:, 1:]
         for idx, row in worker_df.iterrows():
             name = row["Name"]
             certification = row["Equipment Certification(s)"]
             shift = row["Shifts"]
             worker = Worker(name, certification, shift)
-            self.workers.append(worker)
-        # print(self.workers)
+            workers.append(worker)
+        return workers
     
     def initialize_work_orders(self, workOrder_file):
+        work_orders = []
         workOrder_df = pd.read_csv(workOrder_file, header=1).iloc[:, 1:]
         for idx, row in workOrder_df.iterrows():
-            pass
+            equipment = row["Equipment Type"]
+            priority = row["Priority(1-5)"]
+            duration = row["Time to Complete"]
+            submission_time = row["Submission Timestamp"]
+            work_order = WorkOrder(self.workOrder_id, equipment, priority, duration, submission_time)
+            self.workOrder_id += 1
+            work_orders.append(work_order)
+        return work_orders
+
+    def update_work_state(self, action):
+        pass
+    
+    def 
 
 
 
 class Equipment:
-    def __init__(self, name, id, prob_failure, fixing_time, status="idle"):
+    def __init__(self, name, id, prob_failure, fixing_time, status=0):
         self.name = name
         self.prob_failure = prob_failure
         self.fixing_time = fixing_time
@@ -83,11 +89,11 @@ class Equipment:
     def get_fixing_time(self):
         return random.randint(*self.fixing_time)
 
-    def put_towork(self):
-        self.status = "busy"
+    def put_towork(self, hours):
+        self.status = hours
     
     def is_available(self):
-        return self.status == "idle"
+        return self.status == 0
 
 
 class Facility:
@@ -105,20 +111,19 @@ class Facility:
         # print(self.equipments)
 
     def has_available_equipment(self, equip_name):
-        available = False
         for equip in self.equipments[equip_name]:
             if equip.is_available():
                 return True
         return False
 
-    def put_equipment_towork(self, equip_name):
+    def put_equipment_towork(self, equip_name, hours):
         for equip in self.equipments[equip_name]:
             if equip.is_available():
-                equip.put_towork()
+                equip.put_towork(hours)
 
 
 class Worker:
-    def __init__(self, name, certification, shift, status="idle"):
+    def __init__(self, name, certification, shift, status=0):
         self.name = name
         self.certification = certification
         self.shift = shift
@@ -128,19 +133,29 @@ class Worker:
         self.status = new_status
     
     def is_available(self):
-        return self.status == "idle"
+        return self.status == 0
 
-    def put_towork(self):
-        self.status = "busy"
+    def put_towork(self, hours):
+        self.status = hours
     
 
 class WorkOrder:
-    def __init__(self, id, equipment, priority, duration, submission_time):
+    def __init__(self, id, equipment, priority, duration, submission_time, status="pending"):
         self.id = id
         self.equipment = equipment
         self.priority = priority
         self.duration = duration
         self.submission_time = submission_time
+        self.status = status
+    
+    def is_pending(self):
+        return self.status == "pending"
+    
+    def is_in_progress(self):
+        return self.status == "in progress"
+    
+    def is_finished():
+        return self.status == "finished"
 
 
 if __name__ == '__main__':
