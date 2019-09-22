f73aebab294277225f1c972b1f7086250e006d96
JLiao980706 <liaofangshuo@gmail.com>
2019-09-22 05:18:34 -0500

    codes update

--- backend/qlearningAgent.py
+++ backend/qlearningAgent.py
@@ -5,13 +5,13 @@ from qScoreModel import LinearQScore
 
 class QLearningAgent:
 
-    def __init__(self, discount, lr, epsilon, num_training, sess, is_training=False):
+    def __init__(self, discount, lr, epsilon, num_training, is_training=False):
         self.is_training = is_training
         self.discount = discount
         self.epsilon = epsilon
         self.evoke_envir = None
         self.num_training = num_training
-        self.q_value_model = LinearQScore(110, lr, sess)
+        self.q_value_model = LinearQScore(110, lr)
         self.cur_episode = 0
         self.equips = ['Pump', 'Compressor', 'Seperator', 'Sensor', 'Security', 'Electricity', 'Networking', 'Vehicle', 'HVAC', 'Conveyer']
         self.num_equips = len(self.equips)
