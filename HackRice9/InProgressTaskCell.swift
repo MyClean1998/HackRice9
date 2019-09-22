97a4e0b9ff087945131493419dd0fad6370e326a
Rguo18 <rg52@rice.edu>
2019-09-22 01:17:02 -0500

    added inProgressTaskController and the cell

---
+++ HackRice9/InProgressTaskCell.swift
@@ -0,0 +1,15 @@
+//
+//  InProgressTaskCell.swift
+//  HackRice9
+//
+//  Created by macpro on 2019/9/22.
+//  Copyright © 2019 GoldenDumplings. All rights reserved.
+//
+
+import UIKit
+
+class InProgressTaskCell: UICollectionViewCell {
+    
+    @IBOutlet weak var taskNum: UILabel!
+    @IBOutlet weak var availableWorkers: UILabel!
+}
